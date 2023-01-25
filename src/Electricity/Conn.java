cad6af8cc28d958c146b0d825a9c619455c3c40e
Shivam Kumar Maurya <shivam05046@gmail.com>
2023-01-25 18:57:56 +0530

    first commit

---
+++ src/Electricity/Conn.java
@@ -0,0 +1,17 @@
+package Electricity;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///ebs","root","Mavi2425@#");    
+            s =c.createStatement();
+        }catch(Exception e){ 
+            System.out.println(e);
+        }
+    }  
+}  
